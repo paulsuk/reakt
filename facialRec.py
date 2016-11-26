8004d53cf5ed4183dc006a64195fa7248408959a
paulsuk <paul.suk@mail.utoronto.ca>
2016-11-26 07:40:26 -0500

    can sample images from file, and get data for images

--- facialRec.py
+++ facialRec.py
@@ -1,13 +1,21 @@
-from SimpleCV import Camera
 import base64
 import time
 import os
 from googleapiclient import discovery
 from oauth2client.client import GoogleCredentials
 from PIL import Image
+from moviepy.editor import *
+import glob
 
 
 GOOGLE_APPLICATION_CREDENTIALS = "GOOGLE_APPLICATION_CREDENTIALS.json"
+LIKELY = "LIKELY"
+VERY_LIKELY = "VERY_LIKELY"
+
+class audience_data:
+    def __init__(self, time, results):
+        self.time = time
+        self.results = results
 
 def set_up_credentials():
     dir_path = os.path.dirname(os.path.realpath(__file__))
@@ -44,7 +52,12 @@ def detect_face(image, max_results=10):
         })
 
     response = request.execute()
-
+    if not response['responses']:
+        return []
+    elif not response['responses'][0]:
+        return []
+    elif not response['responses'][0]['faceAnnotations']:
+        return []
     return response['responses'][0]['faceAnnotations']
 
 def audience_response(faces):
@@ -52,63 +65,59 @@ def audience_response(faces):
                     'sorrow': 0,
                     'anger': 0,
                     'surprise': 0,
-                    'neutral': 0,
-                    'total': 0.0}
+                    'neutral': 0}
+    if not faces:
+        return num_emotions
+    total = 0
     for face in faces:
         emotion = False
-        if face.joyLikelihood == LIKELY or face.joyLikelihood == VERY_LIKELY:
+        if face['joyLikelihood'] == LIKELY or face['joyLikelihood'] == VERY_LIKELY:
             num_emotions['joy'] += 1
             emotion = True
-        if face.sorrowLikelihood == LIKELY or face.sorrowLikelihood == VERY_LIKELY:
+        if face['sorrowLikelihood'] == LIKELY or face['sorrowLikelihood'] == VERY_LIKELY:
             num_emotions['sorrow'] += 1
             emotion = True
-        if face.angerLikelihood == LIKELY or face.angerLikelihood == VERY_LIKELY:
+        if face['angerLikelihood'] == LIKELY or face['angerLikelihood'] == VERY_LIKELY:
             num_emotions['anger'] += 1
             emotion = True
-        if face.surpriseLikelihood == LIKELY or face.surpriseLikelihood == VERY_LIKELY:
+        if face['surpriseLikelihood'] == LIKELY or face['surpriseLikelihood'] == VERY_LIKELY:
             num_emotions['surprise'] += 1
             emotion = True
-        if !emotion:
+        if emotion == False:
             num_emotions['neutral'] += 1
-        num_emotions['total'] += 1
+        total += 1
 
     per_emotions = {}
     for emotion, value in num_emotions.iteritems():
-        per_emotions[emotion] = value / num_emotions['total']
-    per_emotions['total'] = num_emotions['total']
+        per_emotions[emotion] = value / total
     return per_emotions
 
-class audience_data:
-    def __init__(self, time, results):
-        self.time = time
-        self.results = results
+def getVideoClipResults(fileName, framerate):
+    clip = VideoFileClip(fileName)
+    clip.write_images_sequence('frame%03d.png', framerate)
+    samples = make_img_list()
+    final_results = []
+    for i in range(len(samples)):
+        image = open(samples[i], 'rb')
+        data = getResultsFromSample(image, i/framerate)
+        final_results.append(data)
+    return final_results
+
+def getResultsFromSample(sampleImage, timeStamp):
+    faces = detect_face(sampleImage)
+    results = audience_response(faces)
+    data = audience_data(timeStamp, results)
+    return data
+
+def make_img_list():
+    #Make list of image filenames
+    image_list = []
+    for filename in glob.glob('*.png'):
+        image_list.append(filename)
+    return image_list
 
 if __name__ == "__main__":
+    fileName = 'video.mp4'
+    framerate = 2
     set_up_credentials()
-    '''
-    image_name = "crowdofpeople.jpg"
-    image = open(image_name, 'rb')
-    '''
-    storage = []
-    # Initialize the camera
-    cam = Camera()
-    initial_time = time.clock()
-    # Loop to continuously get images
-    while True:
-        time.sleep(1)
-        # Get Image from camera
-        img = cam.getImage()
-        time = time.clock()
-        # Detect faces
-        faces = detect_face(img)
-        #Calculate results
-        results = audience_response(faces)
-        data = audience_data(time-initial_time, results)
-        storage.append(data)
-        print_results = ""
-        for emotion, value in results.iteritems():
-            print_results += emotion + ': ' + str(value) + '%\n'
-        # Draw info text on image
-        img.drawText(print_results)
-        # Show the image
-        img.show()
+    final = getVideoClipResults(fileName, framerate)
